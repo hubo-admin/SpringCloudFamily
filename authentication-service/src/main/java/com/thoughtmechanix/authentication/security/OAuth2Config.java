3699660db476a5f65ebe052f76fce8c83db264c7
hubo-admin <2249535014@qq.com>
2021-05-21 15:04:20 +0800

    update

--- authentication-service/src/main/java/com/thoughtmechanix/authentication/security/OAuth2Config.java
+++ authentication-service/src/main/java/com/thoughtmechanix/authentication/security/OAuth2Config.java
@@ -22,6 +22,9 @@ import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
 import java.util.Arrays;
 
+/**
+ * 定义哪些服务在认证服务中进行了注册 即:定义了什么应用程序可以使用你的服务
+ */
 @Configuration
 public class OAuth2Config extends AuthorizationServerConfigurerAdapter {
 
@@ -31,19 +34,30 @@ public class OAuth2Config extends AuthorizationServerConfigurerAdapter {
     @Autowired
     private UserDetailsService userDetailsService;
 
+    /**
+     * 定义了哪些客户端将被你的服务注册
+     * @param clients
+     * @throws Exception
+     */
     @Override
     public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
         clients.inMemory()
                 .withClient("eagleeye")
                 .secret("thisissecret")
+                //支持密码呾客户凭证授权
                 .authorizedGrantTypes("refresh_token", "password", "client_credentials")
-                .scopes("webclient", "mobileclient");
+                .scopes("webclient","mobileclient");
     }
 
+    /**
+     * 定义AuthenticationServerConfigurer使用的不同组件
+     *      Spring 使用 Spring 自带的默认身份验证管理器和用户详细信息服务。
+     * @param endpoints
+     * @throws Exception
+     */
     @Override
     public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
-        endpoints
-                .authenticationManager(authenticationManager)
+        endpoints.authenticationManager(authenticationManager)
                 .userDetailsService(userDetailsService);
     }
 }
--- authentication-service/src/main/java/com/thoughtmechanix/authentication/security/WebSecurityConfigurer.java
+++ authentication-service/src/main/java/com/thoughtmechanix/authentication/security/WebSecurityConfigurer.java
@@ -1,5 +1,6 @@
 package com.thoughtmechanix.authentication.security;
 
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -7,28 +8,49 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 
-
+/**
+ * 扩展 Spring Security 核心的 WebSecurityConfigurerAdapter
+ */
 @Configuration
 public class WebSecurityConfigurer extends WebSecurityConfigurerAdapter {
+
+    /**
+     * @Bean 使用 AuthenticationManagerBean 来处理身份验证
+     * @return
+     * @throws Exception
+     */
     @Override
     @Bean
     public AuthenticationManager authenticationManagerBean() throws Exception {
         return super.authenticationManagerBean();
     }
 
+    /**
+     * @Bean 使用 userDetailsServiceBean 来处理 Spring Security 返回的用户信息
+     * @return
+     * @throws Exception
+     */
     @Override
     @Bean
     public UserDetailsService userDetailsServiceBean() throws Exception {
         return super.userDetailsServiceBean();
     }
 
-
+    /**
+     * 定义用户、密码和角色
+     *     在内存中存储了两个用户
+     * @param auth
+     * @throws Exception
+     */
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth
-                .inMemoryAuthentication()
-                .withUser("john.carnell").password("password1").roles("USER")
-                .and()
-                .withUser("william.woodward").password("password2").roles("USER", "ADMIN");
+        auth.inMemoryAuthentication()
+                .withUser("john.carnell")
+                .password("password1")
+                .roles("USER")
+            .and()
+                .withUser("william.woodward")
+                .password("password2")
+                .roles("USER", "ADMIN");
     }
 }
