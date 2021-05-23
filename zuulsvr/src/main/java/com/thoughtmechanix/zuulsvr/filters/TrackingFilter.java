10d0c0641439b8c03d1d5864d26f76411477ee23
hubo-admin <2249535014@qq.com>
2021-05-23 15:35:44 +0800

    网关使jjwt解析令牌

--- zuulsvr/src/main/java/com/thoughtmechanix/zuulsvr/filters/TrackingFilter.java
+++ zuulsvr/src/main/java/com/thoughtmechanix/zuulsvr/filters/TrackingFilter.java
@@ -2,6 +2,9 @@ package com.thoughtmechanix.zuulsvr.filters;
 
 import com.netflix.zuul.ZuulFilter;
 import com.netflix.zuul.context.RequestContext;
+import com.thoughtmechanix.zuulsvr.config.ServiceConfig;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,6 +22,9 @@ public class TrackingFilter extends ZuulFilter{
     @Autowired
     FilterUtils filterUtils;
 
+    @Autowired
+    private ServiceConfig serviceConfig;
+
     public Object run() {
 
         if (isCorrelationIdPresent()) {
@@ -31,8 +37,14 @@ public class TrackingFilter extends ZuulFilter{
         }
 
         RequestContext ctx = RequestContext.getCurrentContext();
+
+        //输出从JWT令牌中解析的数据 organizationID
+        System.out.println(" ---->>> The organization id from the token is : " + getOrganizationId());
+        filterUtils.setOrgId(getOrganizationId());
+
         //日志输出访问来源地址
         logger.debug("Processing incoming request for {}.",  ctx.getRequest().getRequestURL() + ctx.getRequest().getRequestURI());
+
         return null;
     }
     @Override
@@ -64,5 +76,27 @@ public class TrackingFilter extends ZuulFilter{
      */
     private String generateCorrelationId(){
         return java.util.UUID.randomUUID().toString();
+    }
+
+    private String getOrganizationId(){
+
+        String result="";
+        if (filterUtils.getAuthToken()!=null){
+            // 从 Http获取首部 Authorization 并解析过户临牌部分
+            String authToken = filterUtils.getAuthToken()
+                    .replace("Bearer ","");
+            // 利用密钥和JETS类解析数据
+            try {
+                Claims claims = Jwts.parser()
+                        .setSigningKey(serviceConfig.getJwtSigningKey().getBytes("UTF-8"))
+                        .parseClaimsJws(authToken).getBody();
+                //从数据中提取数据
+                result = (String) claims.get("organizationId");
+            }
+            catch (Exception e){
+                e.printStackTrace();
+            }
+        }
+        return result;
     }
 }
