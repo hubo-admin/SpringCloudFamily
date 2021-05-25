95c709930acb7ed2c0d5a385a6a977d14f41fd52
hubo-admin <2249535014@qq.com>
2021-05-25 22:10:48 +0800

    远程调用组织服务时怎加缓存层

--- licensing-service/src/main/java/com/thoughtmechanix/licenses/clients/OrganizationRestTemplateClient.java
+++ licensing-service/src/main/java/com/thoughtmechanix/licenses/clients/OrganizationRestTemplateClient.java
@@ -1,6 +1,7 @@
 package com.thoughtmechanix.licenses.clients;
 
 import com.thoughtmechanix.licenses.model.Organization;
+import com.thoughtmechanix.licenses.repository.OrganizationRedisRepository;
 import com.thoughtmechanix.licenses.utils.UserContextHolder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -28,6 +29,27 @@ public class OrganizationRestTemplateClient {
 //    @Autowired
 //    OAuth2RestTemplate restTemplate;
 
+    @Autowired
+    OrganizationRedisRepository orgRedisRepo;
+
+    private Organization checkRedisCache(String organizationId) {
+        try {
+            return orgRedisRepo.findOrganization(organizationId);
+        }
+        catch (Exception ex){
+            logger.error("Error encountered while trying to retrieve organization {} check Redis Cache.  Exception {}", organizationId, ex);
+            return null;
+        }
+    }
+
+    private void cacheOrganizationObject(Organization org) {
+        try {
+            orgRedisRepo.saveOrganization(org);
+        }catch (Exception ex){
+            logger.error("Unable to cache organization {} in Redis. Exception {}", org.getId(), ex);
+        }
+    }
+
     public Organization getOrganization(String organizationId){
 //        ResponseEntity<Organization> restExchange =
 //                restTemplate.exchange(
@@ -35,14 +57,30 @@ public class OrganizationRestTemplateClient {
 //                        HttpMethod.GET,
 //                        null, Organization.class, organizationId);
 
-        //通过zuul网关代理
         logger.debug(">>> In Licensing Service.getOrganization: {}. Thread Id: {}", UserContextHolder.getContext().getCorrelationId(), Thread.currentThread().getId());
+
+        //从缓存中获取
+        Organization org = checkRedisCache(organizationId);
+        if (org!=null){
+            logger.debug("I have successfully retrieved an organization {} from the redis cache: {}", organizationId, org);
+            return org;
+        }
+
+        //通过zuul网关代理 （缓存未命中时，通过网关到组织服务查询）
         ResponseEntity<Organization> restExchange =
                 restTemplate.exchange(
                         "http://zuulservice/api/organization/v1/organizations/{organizationId}",
                         HttpMethod.GET,
                         null, Organization.class, organizationId);
 
-        return restExchange.getBody();
+        /*Save the record from cache*/
+        org = restExchange.getBody();
+        //从组织服务查询到数据，缓存到Redis
+        if (org!=null) {
+            cacheOrganizationObject(org);
+            logger.debug("save to redis {} : {}", organizationId, org);
+        }
+
+        return org;
     }
 }
