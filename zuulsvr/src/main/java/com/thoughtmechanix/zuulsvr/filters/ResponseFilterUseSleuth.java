91b2c427f0d0ee3716f9efa1fcae3e0aa0b895bd
hubo-admin <2249535014@qq.com>
2021-05-27 20:34:49 +0800

    修改zuul后置过滤器，使用跟踪作为管理id传递

--- zuulsvr/src/main/java/com/thoughtmechanix/zuulsvr/filters/ResponseFilterUseSleuth.java
+++ zuulsvr/src/main/java/com/thoughtmechanix/zuulsvr/filters/ResponseFilterUseSleuth.java
@@ -6,28 +6,31 @@ import com.netflix.zuul.context.RequestContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.sleuth.Tracer;
 import org.springframework.stereotype.Component;
 
 /**
  * @Description: 后置过滤器，接收关联 ID
  **/
 @Component
-public class ResponseFilter extends ZuulFilter{
-    private static final int  FILTER_ORDER=1;
-    private static final boolean  SHOULD_FILTER=false;
-    private static final Logger logger = LoggerFactory.getLogger(ResponseFilter.class);
+public class ResponseFilterUseSleuth extends ZuulFilter{
+    private static final int  FILTER_ORDER=2;
+    private static final boolean  SHOULD_FILTER=true;
+    private static final Logger logger = LoggerFactory.getLogger(ResponseFilterUseSleuth.class);
     
     @Autowired
     FilterUtils filterUtils;
 
+    @Autowired
+    Tracer tracer;
+
     @Override
     public Object run() {
         RequestContext ctx = RequestContext.getCurrentContext();
-        //向响应中添加 关联ID
-        logger.debug("Adding the correlation id to the outbound headers. {}", filterUtils.getCorrelationId());
+        //向响应中添加 关联ID (添加sleuth中的跟踪id)
         ctx.getResponse().addHeader(
                 FilterUtils.CORRELATION_ID,
-                filterUtils.getCorrelationId());
+                tracer.getCurrentSpan().traceIdString());
 
         logger.debug("Completing outgoing request for {}.", ctx.getRequest().getRequestURL() + ctx.getRequest().getRequestURI());
 
